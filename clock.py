3cc3f7c22c084e1826145ae01c5bcf09877826d4
EyeScary development studio <120305267+Pittab@users.noreply.github.com>
2024-02-21 07:03:58 +0000

    Added clock
    A 10 second sample of the date and time (time in app menu)

---
+++ clock.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import time
+clock = datetime.now()
+
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+clock= datetime.now()
+print("the date and time is", clock)
+time.sleep(1)
+
+command=input("return to home? y/n ")
+if command == "y":
+    import main
+else:
+    print("quitting...")
