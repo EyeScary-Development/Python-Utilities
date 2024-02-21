449b5208fabce23f33f4e6ba787c298c836b16a4
EyeScary development studio <120305267+Pittab@users.noreply.github.com>
2024-02-21 07:03:11 +0000

    Update main.py
    Added additional features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("Welcome to python utilities. our apps:")
 print("painted cube solving tool")
 print("triangle area finder")
 print("slime fighter(game)")
+print("time (10 second sample of the date and time)")
 command=input("which app do you want to use? ")
 
 if command == "painted cube solving tool":
@@ -10,4 +11,7 @@ else:
   if command == "triangle area finder":
     import triongle
   else:
-    import pidms
+    if command == "time":
+      import clock
+    else:
+      import pidms
