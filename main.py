ff7e7d2cc162c95e180541176e21e770e3bf6d54
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-02-22 11:25:14 +0000

    Update main.py to make it more readable

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ print("painted cube solving tool")
 print("triangle area finder")
 print("slime fighter(game)")
 print("time (10 second sample of the date and time)")
-command=input("which app do you want to use? ")
+# Above prints what programs you can access, Below asks the user which program they want to access
+command = input("which app do you want to use? ")
 
-if command == "painted cube solving tool":
+if command == "painted cube solving tool": # If input = programs, run the program
   import paintedcubesolver
 else:
   if command == "triangle area finder":
