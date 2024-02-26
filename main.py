6aa80259ef73c8125c4f78ee84d5b4f515913bd4
Pittab <120305267+Pittab@users.noreply.github.com>
2024-02-26 17:13:11 +0000

    Update main.py
    Accomodate dictionary

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print("slime fighter(game)")
 print("time (10 second sample of the date and time)")
 print("2d area calculator")
 print("3d volume calculator")
+print("dictionary")
 command=input("which app do you want to use? ")
 
 if command == "painted cube solving tool":
@@ -13,9 +14,11 @@ elif command == "triangle area finder":
   import triongle
 elif command == "time": 
     import clock
-elif: command == "2d area calculator":
+elif command == "2d area calculator":
   import arcalc
-elif: command == "3d volume calculator":
+elif command == "3d volume calculator":
   import volcalc
+elif command == "dictionary":
+  import diction
 else:
       import pidms
