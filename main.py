9bc603ca93764672648fc2ba99b926a647356b30
EyeScary development studio <120305267+Pittab@users.noreply.github.com>
2024-02-26 07:36:49 +0000

    Update main.py
    streamlined code and added options for arcalc and volcanic

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ print("painted cube solving tool")
 print("triangle area finder")
 print("slime fighter(game)")
 print("time (10 second sample of the date and time)")
+print("2d area calculator")
+print("3d volume calculator")
 command=input("which app do you want to use? ")
 
 if command == "painted cube solving tool":
   import paintedcubesolver
+elif command == "triangle area finder":
+  import triongle
+elif command == "time": 
+    import clock
+elif: command == "2d area calculator":
+  import arcalc
+elif: command == "3d volume calculator":
+  import volcalc
 else:
-  if command == "triangle area finder":
-    import triongle
-  else:
-    if command == "time":
-      import clock
-    else:
       import pidms
