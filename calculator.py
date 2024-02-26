367605bfce6a5f42252ac28822ad6566b4654ed1
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-02-26 17:36:14 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-Operation = input("Operations are \n Addition (Type 1) \n Subtraction (Type 2) \n Division (Type 3) \n Multiplication (Type 4) \n So which operation do you want to use?: ")
+Operation = input("Operations are \n Addition (Type 1) \n Subtraction (Type 2) \n Division (Type 3) \n Multiplication (Type 4) \n Square Num 1 (Type 5) \n So which operation do you want to use?: ")
 Num1 = int(input("What is the first number you want to use this operation with: "))
 Num2 = int(input("What is the second number you want to use this operation with: "))
 if Operation == "1":
@@ -9,6 +9,8 @@ elif Operation == "3":
     print(Num1," / ",Num2," = ",Num1 / Num2)
 elif Operation == "4":
     print(Num1," * ",Num2," = ",Num1 * Num2)
+elif Operation == "5":
+    print(Num1," squared is ",Num1 * Num1)
 else:
     print("Not Valid")
 
