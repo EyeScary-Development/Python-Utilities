ea1fd8d701610ef7a4e6d427eec09e628eb52def
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-09-18 12:34:02 +0100

    Update calculator.py - Made the code easier to read

--- calculator.py
+++ calculator.py
@@ -1,26 +1,27 @@
-Operation = input("Operations are \n Addition (Type 1) \n Subtraction (Type 2) \n Division (Type 3) \n Multiplication (Type 4) \n Square Num 1 (Type 5) \n Cube Num1 (Type 6) \n Quadanteral (Type 7) \n So which operation do you want to use?: ")
-Num1 = int(input("What is the first number you want to use this operation with: "))
-Num2 = int(input("What is the second number you want to use this operation with: "))
-if Operation == "1":
-    print(Num1," + ",Num2," = ",Num1 + Num2)
-elif Operation == "2":
-    print(Num1," - ",Num2," = ",Num1 - Num2)
-elif Operation == "3":
-    print(Num1," / ",Num2," = ",Num1 / Num2)
-elif Operation == "4":
-    print(Num1," * ",Num2," = ",Num1 * Num2)
-elif Operation == "5":
-    print(Num1," squared is ",Num1 * Num1)
-elif Operation == "6":
-    print(Num1," cubed is ",Num1*(Num1*Num1))
-elif Operation == "7":
-    print(Num1," quadanteraled is ",(Num1*(Num1*Num1))*Num1)
-else:
-    print("Not Valid")
+command = input("Would you like to go home? \n y/n").
 
-command=input("would you like to go home? y/n: ")
+while command == "y":
+    Operation = input("Operations are \n Addition (Type 1) \n Subtraction (Type 2) \n Division (Type 3) \n Multiplication (Type 4) \n Square Num 1 (Type 5) \n Cube Num1 (Type 6) \n Power of 4 (Type 7) \n So which operation do you want to use?: ")
+    Num1 = int(input("What is the first number you want to use this operation with: "))
+    Num2 = int(input("What is the second number you want to use this operation with: "))
+    if Operation == "1":
+        print(Num1," + ",Num2," = ",Num1 + Num2)
+    elif Operation == "2":
+        print(Num1," - ",Num2," = ",Num1 - Num2)
+    elif Operation == "3":
+        print(Num1," / ",Num2," = ",Num1 / Num2)
+    elif Operation == "4":
+        print(Num1," * ",Num2," = ",Num1 * Num2)
+    elif Operation == "5":
+        print(Num1," squared is ",Num1 * Num1)
+    elif Operation == "6":
+        print(Num1," cubed is ",Num1*(Num1*Num1))
+    elif Operation == "7":
+        print(Num1," to the power of 4 is ",(Num1*(Num1*Num1))*Num1)
+    else:
+        print("Not Valid")
+    
+    command=input("would you like to go home? y/n: ")
 
-if command == "y":
-    import main
-else:
-    print("quitting...")
+import main
+
